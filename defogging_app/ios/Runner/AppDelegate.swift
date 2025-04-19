1f29d093de11f583fd51b4811401f61ffd0e8053
Steak Fairy <wangqijie77@gmail.com>
2025-04-19 16:29:06 +0100

    Updated dependencies, added Google Maps and location features, modified Android and iOS configurations, enhanced build settings, optimised app theme and homepage.

--- defogging_app/ios/Runner/AppDelegate.swift
+++ defogging_app/ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyCPb97FNTwxymYWAtAcELsIvkIgOlsiWzE")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
---
+++ defogging_app/lib/pages/google_map_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class GoogleMapPage extends StatefulWidget {
+  const GoogleMapPage({super.key});
+
+  @override
+  State<GoogleMapPage> createState() => _GoogleMapPageState();
+}
+
+class _GoogleMapPageState extends State<GoogleMapPage> {
+  late GoogleMapController mapController;
+  final LatLng _center = const LatLng(51.5074, -0.1278); // 伦敦坐标
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  void dispose() {
+    mapController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: _center,
+          zoom: 11.0,
+        ),
+        myLocationEnabled: true,
+        myLocationButtonEnabled: true,
+        mapType: MapType.normal,
+      ),
+    );
+  }
+}
